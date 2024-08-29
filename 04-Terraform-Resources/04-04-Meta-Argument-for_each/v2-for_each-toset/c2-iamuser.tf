dbe76f7c325194f738b3632bb34b29754fe5bb4d
Sohail130 <84300969+Sohail130@users.noreply.github.com>
2024-08-30 02:02:15 +0530

    Update c2-iamuser.tf

--- 04-Terraform-Resources/04-04-Meta-Argument-for_each/v2-for_each-toset/c2-iamuser.tf
+++ 04-Terraform-Resources/04-04-Meta-Argument-for_each/v2-for_each-toset/c2-iamuser.tf
@@ -5,3 +5,24 @@ resource "aws_iam_user" "myuser" {
   for_each = toset(["TJack", "TJames", "TMadhu", "TDave"])
   name     = each.key
 }
+
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+  policy      = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  users      =  values(aws_iam_user.myuser)[*].name
+  policy_arn = aws_iam_policy.policy.arn
+}
